49c77fd55977ab53d974bafd2aec797de995a2b6
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-09-28 17:37:46 -0300

    initial config for the store in the index.js file

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import BooksApp from './reducers/BooksApp';
 import Books from './components/containers/Books';
+import { Provider } from 'react-redux';
 
 class App extends React.Component {
     render() {
         return (
             <div>
-                <Books />
+                <Books store={store}/>
             </div>
         )
     }
@@ -19,6 +20,8 @@ let store = createStore(BooksApp);
 console.log(store.getState());
 
 ReactDOM.render(
-    <App/>,
+    <Provider store = {store}>
+        <App/>
+    </Provider>,
     document.getElementById('app')
 );
