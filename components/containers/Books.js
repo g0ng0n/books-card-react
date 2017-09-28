ea75bfef58e6b9a9d92699c088e1992fc36c6c8b
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-09-28 17:41:57 -0300

    adding the Books Container Component

---
+++ components/containers/Books.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { getState } from 'redux';
+
+import BookCard from '../presentationals/BookCard';
+
+//Provider/Container React Component
+
+class Books extends React.Component {
+    render() {
+        const stateProps = this.props.store.getState();
+        console.log(stateProps);
+
+        return (
+            <div>
+                <BookCard stateProps = { stateProps }/>
+            </div>
+        )
+    }
+}
+
+export default Books
