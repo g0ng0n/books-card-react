f5b2b3ab025039349209328986e2275dfcb22d34
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-09-29 14:58:54 -0300

    adding code implementation in order to handle the actions in the book cards

--- components/containers/Books.js
+++ components/containers/Books.js
@@ -1,18 +1,48 @@
 import React from 'react'
 import { getState } from 'redux';
 
+import AddBook from '../../actions/AddBook';
+import DeleteBook from '../../actions/DeleteBook';
+import EditBook from '../../actions/EditBook';
 import BookCard from '../presentationals/BookCard';
 
 //Provider/Container React Component
 
 class Books extends React.Component {
+
+    dispatchAction (input){
+        switch (input){
+            case "TRASH":
+                this.props.store.dispatch(DeleteBook());
+                break;
+            case "PLUS":
+                this.props.store.dispatch(AddBook());
+                break;
+            case "PENCIL":
+                this.props.store.dispatch(EditBook());
+                break;
+        }
+    }
+
+    componentWillMount() {
+        this.props.store.subscribe(this.forceUpdate.bind(this));
+    }
+
+
     render() {
         const stateProps = this.props.store.getState();
         console.log(stateProps);
 
+        const bookItems = stateProps.books.map((book) =>
+            <BookCard key = { book }
+                      stateProps = { stateProps }
+                      dispatchAction= {this.dispatchAction.bind(this)}
+            />
+        );
+
         return (
             <div className="books-container">
-                <BookCard stateProps = { stateProps }/>
+               { bookItems }/>
             </div>
         )
     }
--- components/presentationals/BookCard.js
+++ components/presentationals/BookCard.js
@@ -9,6 +9,10 @@ class BookCard extends React.Component {
                 <h2>{this.props.stateProps.title}</h2>
                 <h3>{this.props.stateProps.author}</h3>
                 <a href={this.props.stateProps.link}>{this.props.stateProps.link}</a>
+                <br/>
+                <i onClick={() => this.props.dispatchAction("PENCIL")} className="icon fa fa-pencil" aria-hidden="true"></i>
+                <i onClick={() => this.props.dispatchAction("TRASH")} className="icon fa fa-trash" aria-hidden="true"></i>
+                <i onClick={() => this.props.dispatchAction("PLUS")} className="icon fa fa-plus" aria-hidden="true"></i>
             </div>
         )
     }
